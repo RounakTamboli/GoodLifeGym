11c30364c9aa64a6c3c68d651e04566672404eb9
RounakTamboli <rounaktamboli346@gmail.com>
2025-08-21 23:11:18 -0700

    done

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// Simple beginner JavaScript
-console.log("GoodLife Gym Website Loaded!");
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("All fields must be filled out!");
+    return false;
+  }
+  alert("Form submitted successfully!");
+  return true;
+}
